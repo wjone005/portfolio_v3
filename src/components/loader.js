beab20bcdd8a57c5d50ee150fcca75c50853fa17
Willie Jones III <williejones005@gmail.com>
2025-02-13 00:20:08 -0800

    Updated the B to a W

--- src/components/loader.js
+++ src/components/loader.js
@@ -31,7 +31,7 @@ const StyledLogo = styled.div`
     margin: 0 auto;
     fill: none;
     user-select: none;
-    #B {
+    #W {
       opacity: 0;
     }
   }
@@ -52,7 +52,7 @@ const Loader = ({ finishLoading }) => {
         strokeDashoffset: [anime.setDashoffset, 0],
       })
       .add({
-        targets: '#logo #B',
+        targets: '#logo #W',
         duration: 700,
         easing: 'easeInOutQuart',
         opacity: 1,
